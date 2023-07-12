49f223abab7785eed4e079ce47de5ba69a63735e
gemafajar09 <gemafajar09@gmail.com>
2023-07-13 00:29:42 +0700

    latihan

---
+++ resources/js/Pages/Post/Create.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { Inertia } from "@inertiajs/inertia";
+import { InertiaLink } from "@inertiajs/inertia-react";
+
+export default function Create({errors}) {
+    const [title, setTitle] = useState("");
+    const [description, setdescription] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        Inertia.post(route("posts.store"),
+        {
+            title : title,
+            description : description
+        });
+    }
+
+    return (
+        <div className="mt-20">
+            <div className="container flex flex-col justify-center mx-auto">
+                <div>
+                    <h1 className="mb-8 text-3xl font-bold">
+                        <InertiaLink
+                            href={route("posts.index")}
+                            className="text-indigo-600 hover:text-indigo-700"
+                        >
+                            Latihan
+                        </InertiaLink>
+                        <span className="font-medium text-indigo-600"> / </span>
+                        Create
+                    </h1>
+                </div>
+                <div className="max-w-6xl p-8 bg-white rounded shadow">
+                    <form name="createForm" onSubmit={handleSubmit}>
+                        <div className="flex flex-col">
+                            <div className="mb-4">
+                                <label className="">Title</label>
+                                <input
+                                    type="text"
+                                    className="w-full px-4 py-2"
+                                    label="Title"
+                                    name="title"
+                                    value={title}
+                                    onChange={(e) =>
+                                        setTitle(e.target.value)
+                                    }
+                                />
+                                <span className="text-red-600">
+                                    {errors.title}
+                                </span>
+                            </div>
+                            <div className="mb-0">
+                                <label className="">Description</label>
+                                <textarea
+                                    type="text"
+                                    className="w-full rounded"
+                                    label="description"
+                                    name="description"
+                                    errors={errors.description}
+                                    value={description}
+                                    onChange={(e) =>
+                                        setdescription(e.target.value)
+                                    }
+                                />
+                                <span className="text-red-600">
+                                    {errors.description}
+                                </span>
+                            </div>
+                        </div>
+                        <div className="mt-4">
+                            <button
+                                type="submit"
+                                className="px-6 py-2 font-bold text-white bg-green-500 rounded"
+                            >
+                                Save
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
